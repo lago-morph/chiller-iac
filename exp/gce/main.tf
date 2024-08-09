c12d68b8da704cf0a8ee23d981d36fce753d442e
Jonathan Manton <jonathan@manton.com>
2024-08-08 19:23:18 -0500

    adding previous experiments

---
+++ exp/gce/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "tf-tut-2fff5ee746a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  tags         = ["web", "dev"]
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
---
+++ exp/gce/outputs.tf
@@ -0,0 +1,6 @@
+output "ip" {
+  value = google_compute_instance.vm_instance.network_interface.0.network_ip
+}
+output "external_ip" {
+  value = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
+}
